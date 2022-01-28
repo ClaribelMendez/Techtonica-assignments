54e8aa83152fffeb1ec4623b615498902c2d1bdd
Claribel <Claribel.D.Mendez@gmail.com>
2022-01-27 20:44:35 -0500

    created js functions

---
+++ RecipePage/script.js
@@ -0,0 +1,49 @@
+// document.getElementById("btn").addEventListener("click", function() {
+//     var img = document.createElement("img");
+//     img.src = https://c.tenor.com/g_a_ehluKycAAAAC/minions-yay.gif;
+//     img.width = 300;
+//     img.height = 250;
+//     img.alt = Minions happy;
+
+//     document.body.appendChild(img);
+   
+    // alert("YOU DID IT!");
+//   });
+
+
+//   function show_image(src, width, height, alt) {
+//     var img = document.createElement("img");
+//     img.src = src;
+//     img.width = width;
+//     img.height = height;
+//     img.alt = alt;
+
+//     // This next line will just add it to the <body> tag
+//     document.body.appendChild(img);
+// }
+// show_image()
+
+
+
+function showImage() {
+    document.getElementById('loadingImage').style.display = 'block';
+  }
+
+let color = document.getElementById("right");
+color.style.backgroundColor = "red"
+
+// function myFunction() {
+//   let instructionsbackground = document.getElementsByClassName("left")
+//     color.style.backgroundColor = "red";
+// }
+
+// function myFunction() {
+//   document.body.style.backgroundColor = "red";
+// }
+
+// function myFunction() {
+//   document.body.style.backgroundColor = "red";
+// }
+
+
+
