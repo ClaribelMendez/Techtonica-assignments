87c6d0b951d36f739e38296acb7af52fda227681
Claribel <Claribel.D.Mendez@gmail.com>
2022-02-14 18:39:17 -0500

    finalDraft

--- GuessingGame/script.js
+++ GuessingGame/script.js
@@ -1,5 +1,7 @@
-// let guessesLeft = 5
-let randomNumber = 0
+let guessesLeft = document.querySelector('.guessesLeft');
+let enterNumber = document.querySelector(".enterNumber")
+let guessMax = 5;
+let randomNumber = 0;
 let twenty = document.getElementById("20");
 let fourty = document.getElementById("40");
 let sixty = document.getElementById("60")
@@ -10,20 +12,23 @@ twenty.addEventListener('click', chooseRange1);
 fourty.addEventListener('click', chooseRange2);
 sixty.addEventListener('click', chooseRange3);
 
-function chooseRange1(){
-  randomNumber =  Math.floor(Math.random() * (20 - 1) + 1);
-  
+function chooseRange1() {
+  randomNumber = Math.floor(Math.random() * (20 - 1) + 1);
+  fourty.style.display = "none"
+  sixty.style.display = "none"
 }
 
-function chooseRange2(){
+function chooseRange2() {
 
-  randomNumber =  Math.floor(Math.random() * (34-25) + 25);
-  
+  randomNumber = Math.floor(Math.random() * (34 - 25) + 25);
+  twenty.style.display = "none"
+  sixty.style.display = "none"
 }
 
-function chooseRange3(){
-  randomNumber =  Math.floor(Math.random() * (60 - 40) + 40);
-  
+function chooseRange3() {
+  randomNumber = Math.floor(Math.random() * (60 - 40) + 40);
+  fourty.style.display = "none"
+  twenty.style.display = "none"
 }
 
 
@@ -38,59 +43,82 @@ const lowOrHi = document.querySelector(".lowOrHi");
 const guessSubmit = document.querySelector(".guessSubmit");
 const guessField = document.querySelector(".guessField");
 
-let guessCount = 1;
+
 let resetButton;
 
 function checkGuess() {
   const userGuess = Number(guessField.value) // current value entered inside text field.Number is used to verify it's definitely a number
-  if (guessCount === 1) {
+  if (guessMax === 5) {
     guesses.textContent = 'Previous guesses: ';
   }
+  guessesLeft.textContent = 'Guesses left: ' + guessMax;
   guesses.textContent += userGuess + ' '; // puts consecutive guesses togehter and adds a space in between
+  // guessesLeft.textContent + guessMax;
 
   if (userGuess === randomNumber) {
     lastResult.textContent = 'Congratulations! You got it right!';
     lastResult.style.backgroundColor = 'green';
     lowOrHi.textContent = '';
     setGameOver();
-  } else if (guessCount === 5) {
-    lastResult.textContent = '!!!GAME OVER!!!';
-  lowOrHi.textContent = '';
-  setGameOver();
-} else {
-  lastResult.textContent = "Incorrect!";
-  lastResult.style.backgroundColor = "red";
-  if (userGuess < randomNumber) {
-    lowOrHi.textContent = 'Guess higher!';
-  } else if (userGuess > randomNumber) {
-    lowOrHi.textContent = 'Guess lower!'
+
+  } else if (guessMax === 0) {
+    lastResult.textContent = 'GAME OVER!!!';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = "Incorrect!";
+    lastResult.style.backgroundColor = "red";
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = 'Guess higher!';
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = 'Guess lower!'
+    }
   }
-}
 
-guessCount++; 
 
-guessField.value = ''; // empties the value of text field to guess again
-guessField.focus(); // will highlight the border 
+  guessMax--;
+
+  guessField.value = ''; // empties the value of text field to guess again
+  guessField.focus(); // will highlight the border 
 }
 
 guessSubmit.addEventListener('click', checkGuess);
 
 function setGameOver() {
-  guessField.disabled = true;
-  guessSubmit.disabled = true;
+  // guessField.disabled = true;
+  // guessSubmit.disabled = true;
+  twenty.style.display = "none";
+  fourty.style.display = "none";
+  sixty.style.display = "none";
+  guessField.style.display = "none";
+  guessSubmit.style.display = "none";
+  guessesLeft.style.display ="none";
+  guesses.style.display = "none";
+  enterNumber.style.display = "none";
+  
   resetButton = document.createElement('button');
+  resetButton.setAttribute("id", "resetButton");
   resetButton.textContent = 'Start new game';
   document.body.append(resetButton);
   resetButton.addEventListener('click', resetGame);
+ 
+  
 }
 
 function resetGame() {
-  guessCount = 1; // resets nunber of guesses back to 1
-  const resetParas = document.querySelectorAll('.resultText p'); // selects all paragraph tags inside form class 'resultText'.
-  for (const resetPara of resetParas) { // loops through each one
-     resetPara.textContent = ''; // resets all textContent to an empty string
-  
+  guessMax = 5; // resets nunber of guesses back to 5
+  const resultTexts = document.querySelectorAll('.resultText p'); // selects all paragraph tags inside form class 'resultText'.
+  for (const values of resultTexts) { // loops through each one
+    values.textContent = ''; // resets all textContent to an empty string
+
   }
+  twenty.style.display = "inline-block"
+  fourty.style.display = "inline-block"
+  sixty.style.display = "inline-block"
+  guessField.style.display = "inline-block";
+  guessSubmit.style.display = "inline-block";
+  guessesLeft.style.display ="block";
+  guesses.style.display = "block";
   guessField.disabled = false;
   guessSubmit.disabled = false;
   guessField.value = '';
@@ -99,8 +127,6 @@ function resetGame() {
 
   // randomNumber = Math.floor(Math.random()) * 100 + 1; // generates new random number
   resetButton.parentNode.removeChild(resetButton); // removes reset button from code
-  
-
-
 
 }
+
