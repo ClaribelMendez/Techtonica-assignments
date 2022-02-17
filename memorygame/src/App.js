d502f29e8280756f1d460106beb0bc51accf9480
Claribel <Claribel.D.Mendez@gmail.com>
2022-02-17 00:22:26 -0500

    first draft

---
+++ memorygame/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css'
+import Cards from './Components/Cards'
+import Card from './Components/Card'
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Memory Game</h1>
+           
+           
+           
+
+     
+     <Cards />
+     
+    </div>
+  );
+}
+
+export default App;
