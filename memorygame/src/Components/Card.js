1b9608d4b34ebb1756355530cc8b981230e81a6f
Claribel <Claribel.D.Mendez@gmail.com>
2022-02-17 00:15:44 -0500

    adding card images

---
+++ memorygame/src/Components/Card.js
@@ -0,0 +1,9 @@
+function Card(item) {
+    return (
+        <div className = "card">
+            <img src="{item.img}"/>
+        </div>
+    )
+}
+
+export default Card;
---
+++ memorygame/src/Components/Cards.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import Card from './Card'
+
+
+function Cards(){
+    const [items, setItems] = useState([
+        {id: 1, img: 'http://farm9.staticflickr.com/8382/8680588927_17bd4a5e89_z.jpg', stat: ""},
+        {id: 1, img: 'public/Images/IMG_0459.JPG', stat: ""},
+        {id: 2, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 2, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 3, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 3, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 4, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 4, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 5, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 5, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 6, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 6, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 7, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 7, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 8, img: 'image/IMG_0459.JPG', stat: ""},
+        {id: 8, img: 'public/Images/IMG_0460.JPG', stat: ""},
+
+  
+    ])
+
+    return (
+        <div className="container">
+            { items.map((item, index) => {
+               return (
+                    <Card key ={index} item = {item}/>
+
+               )
+            }) }
+        </div>
+    )
+}
+
+export default Cards;
