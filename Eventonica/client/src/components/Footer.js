361a6a0e6934fc1f3d31462938b7d6a34b7f958f
Claribel <Claribel.D.Mendez@gmail.com>
2022-03-14 15:49:17 -0400

    added footer

---
+++ Eventonica/client/src/components/Footer.js
@@ -0,0 +1,20 @@
+function Footer(props) {
+  return (
+    <footer>
+      <div className="footer">
+          <span>{props.text} <a href={props.url}>{props.title} {"       "}</a></span>
+
+        {/* Star Calendar favicon made by
+        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+          Freepik
+        </a>
+        Find your own on
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          FlatIcon.com
+        </a> */}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
