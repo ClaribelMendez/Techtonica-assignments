92c46f413ab1b9c718b7945fef3079831c070192
Claribel <Claribel.D.Mendez@gmail.com>
2022-03-14 23:20:08 -0400

    list of mock user data showing

--- Eventonica/client/src/components/Users.js
+++ Eventonica/client/src/components/Users.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function Users() {
 
-// const marlin = { name: 'Marlin', email: 'marlin@gmail.com', id: '1' };
-// const nemo = { name: 'Nemo', email: 'nemo@gmail.com', id: '2' };
-// const dory = { name: 'Dory', email: 'dory@gmail.com', id: '3' };
+const marlin = { name: 'Marlin', email: 'marlin@gmail.com', id: '1' };
+const nemo = { name: 'Nemo', email: 'nemo@gmail.com', id: '2' };
+const dory = { name: 'Dory', email: 'dory@gmail.com', id: '3' };
+
+const [users, setUsers] = useState([marlin, nemo, dory])
+
+
 
 return (
-
+ 
 
 <section className="user-management">
 <h2>User Management</h2>
 
 <ul id="users-list">
-  {/* display all existing Users here */}
-  <li>...</li>
-</ul>
+ 
+            {users.map(user =>
+            <li key={user.id}> { user.name} {user.email} </li>
+            )}
+        </ul>
 
 <div>
   <h3>Add User</h3>
