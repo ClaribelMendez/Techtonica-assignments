d63cdea59efc1c585400c3692f2b6f90bb02f1cf
Claribel <Claribel.D.Mendez@gmail.com>
2022-03-15 02:12:10 -0400

    Name, Email and ID displaying on Add submit

--- Eventonica/client/src/components/Users.js
+++ Eventonica/client/src/components/Users.js
@@ -7,13 +7,15 @@ const marlin = { name: 'Marlin', email: 'marlin@gmail.com', id: '1' };
 const nemo = { name: 'Nemo', email: 'nemo@gmail.com', id: '2' };
 const dory = { name: 'Dory', email: 'dory@gmail.com', id: '3' };
 
-const [users, setUsers] = useState([marlin, nemo, dory])
-const [name, setName] = useState('')
+const [users, setUsers] = useState([marlin, nemo, dory]);
+const [ name, setName] = useState('');
+const [ email, setEmail] = useState('');
+
 
 
 const handleSubmit = (e) => {
     e.preventDefault();
-    const newUser = {name};
+    const newUser = {name,email};
     setUsers([...users, newUser]);
   };
 
@@ -28,10 +30,11 @@ return (
 
 <ul id="users-list">
  
-            {users.map(user =>
-            <li key={user.id}> { user.name} {user.email}  </li>
+            {users.map((user, index) =>
+            <li key={index}> { user.name} {user.email} </li>
             )}
         </ul>
+
 
 <div>
   <h3>Add User</h3>
@@ -43,8 +46,25 @@ return (
       id="add-user-name"
       value={name}
       onChange={(e) => setName(e.target.value)}
+
+      />
+      <label>Email</label>
+      <input 
+      type="text"
+      id="add-user-email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      />
+
+<label>ID</label>
+      <input 
+      type="text"
+      id="add-user-id"
+      
+      onChange={(e) => setName(e.target.value)}
       />
     </fieldset>
+    
     {/* Add more form fields here */}
     
     <input type="submit" value="Add"  />
